57ec2879dbbcf149b8b379177d4a60149043db96
Carlos Eduardo <carlosed20091@gmail.com>
2025-08-31 16:34:15 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { HeaderComponent } from './components/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { ComodidadesComponent } from './components/comodidades/comodidades.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdsComponent } from './components/ads/ads.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, ComodidadesComponent, AboutComponent, AdsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/comodidades/comodidades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comodidades',
+  imports: [],
+  templateUrl: './comodidades.component.html',
+  styleUrl: './comodidades.component.scss'
+})
+export class ComodidadesComponent {
+
+}
